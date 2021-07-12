cd097db40f8c18a55ff04636e02f9f93183c68e3
drhtka <drhtka@gmil.com>
2021-07-12 13:58:03 +0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Blog(models.Model):
+
+    class Meta:
+        db_table = 'blog'
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блог'
+
+    title = models.CharField('Заголовок', max_length=200)
+    description = models.TextField('Описание',)
+    date = models.DateField('Дата',)
+
+    def __str__(self):
+        return self.title
---
+++ portfolio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Project(models.Model):
+    
+    class Meta:
+        db_table = 'project'
+        verbose_name = 'портфолио'
+        verbose_name_plural = 'портфолио'
+    
+    title = models.CharField('Заголовок', max_length=100)
+    description = models.CharField('Описание', max_length=250)
+    image = models.ImageField('Фото', upload_to='portfolio/images/')
+    url = models.URLField('Ссылка на сайт', blank=True)
+
+    def __str__(self):
+        return self.title
+
